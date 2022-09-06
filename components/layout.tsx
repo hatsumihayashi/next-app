9a007c3df39057a98808d59bb0d800499c743329
Hatsumi Hayashi <hayashihatsumi0114@gmail.com>
2022-09-06 21:50:17 +0900

    仕様的にHeadタグ内でScriptタグが使えないので、GTMタグはAppで読み込ませるようにした

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
-import { GoogleTagManager } from "./gtm";
-
 const name = "Hatsumi Hayashi";
 export const siteTitle = "Next.js Sample Website";
 
@@ -18,7 +16,6 @@ export default function Layout({
   return (
     <div className={styles.container}>
       <Head>
-        <GoogleTagManager googleTagManagerId={process.env.NEXT_PUBLIC_GTM_ID} />
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
